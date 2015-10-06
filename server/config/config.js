3d7f4db31a89deb743f584d9d3db47d757ccffed
neppoliyant <neppoliyant@gmail.com>
2015-10-06 14:43:40 +0000

    config updated

--- server/config/config.js
+++ server/config/config.js
@@ -21,13 +21,13 @@ module.exports = {
         }
     },
     serviceUrl : "http://96.119.5.107:3000/rest/",
-    dir: "/Users/nthang003c/opt/props/bodyBuilding/",
+    dir: "/opt/props/bodyBuilding/",
     connectorUrl: "http://96.119.5.107:3000/rest/user/neps",
     swaggerBaseUrl: "http://96.119.5.107:3000/rest",
     swaggerDocUrl: "http://96.119.5.107:3000",
     couchBase: {
-        server: 'localhost:8091',
-        bucketName: 'bodyBuilding',
-        bucketPassword: 'star_2828'
+        server: 'http://96.119.5.126/:8091',
+        bucketName: 'webrtc-registrar-dev',
+        bucketPassword: 'webrtc'
     }
 }
